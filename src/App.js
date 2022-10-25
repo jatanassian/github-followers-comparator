8569470748f94fd850ad0cda6181fbb7076bdb04
Julien Atanassian <julien.atanassian@gmail.com>
2022-10-25 17:56:34 -0400

    use function instead of useeffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import SearchBox from "./components/search-box/search-box.component";
 import FollowersList from "./components/followers-list/followers-list.component";
 import "./App.css";
@@ -6,31 +6,18 @@ import "./App.css";
 import { parseLinkHeader } from "@web3-storage/parse-link-header";
 
 function App() {
-  const [firstUserFollowers, setFirstUserFollowers] = useState([]);
-  const [secondUserFollowers, setSecondUserFollowers] = useState([]);
   const [commonFollowers, setCommonFollowers] = useState(null);
 
-  // Create array of followers in common when followers arrays are updated
-  useEffect(() => {
-    const followers = firstUserFollowers.filter((follower1) => {
-      return secondUserFollowers.find(
-        (follower2) => follower1.id === follower2.id
-      );
+  // Return an array of common followers
+  const findCommonFollowers = (followers1, followers2) => {
+    const followersInCommon = followers1.filter((follower1) => {
+      return followers2.find((follower2) => follower1.id === follower2.id);
     });
-    setCommonFollowers(followers);
-  }, [firstUserFollowers, secondUserFollowers]);
 
-  const handleSubmit = async (event, searchFields) => {
-    setCommonFollowers(null);
-    const { firstUser, secondUser } = searchFields;
-    event.preventDefault();
-
-    const firstFollowers = await getFollowers(firstUser);
-    const secondFollowers = await getFollowers(secondUser);
-    setFirstUserFollowers(firstFollowers);
-    setSecondUserFollowers(secondFollowers);
+    return followersInCommon;
   };
 
+  // Fetch followers recursively to handle pagination
   const getFollowers = async (username, url = "") => {
     try {
       const res = await fetch(
@@ -42,7 +29,6 @@ function App() {
 
       if (linkHeader) {
         const parsed = parseLinkHeader(linkHeader);
-        // Get followers of next page if next exists
         if (parsed.next?.url) {
           const nextFollowers = await getFollowers(username, parsed.next.url);
           followers.push(...nextFollowers);
@@ -52,6 +38,21 @@ function App() {
     } catch (error) {
       console.log(error);
     }
+  };
+
+  // Get followers, find common followers and set them in the state
+  const handleSubmit = async (event, searchFields) => {
+    setCommonFollowers(null);
+    const { firstUser, secondUser } = searchFields;
+    event.preventDefault();
+
+    const firstFollowers = await getFollowers(firstUser);
+    const secondFollowers = await getFollowers(secondUser);
+    const followersInCommon = findCommonFollowers(
+      firstFollowers,
+      secondFollowers
+    );
+    setCommonFollowers(followersInCommon);
   };
 
   return (
